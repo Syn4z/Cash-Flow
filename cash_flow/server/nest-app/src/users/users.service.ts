47bef8638bd738f1a073103d1f7dba4d2a039e9d
sorin <iatcosorin797@gmail.com>
2022-10-10 16:45:37 +0300

    Add docker-compose and users

--- cash_flow/server/nest-app/src/users/users.service.ts
+++ cash_flow/server/nest-app/src/users/users.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(prismaService: PrismaService) {
+    
+  } 
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
