16a1eec636bf7063b5abfe14e84bddfd5ffd1328
sorin <iatcosorin797@gmail.com>
2022-10-31 17:18:28 +0200

    Repair prisma

--- cash_flow/server/nest-app/src/app.module.ts
+++ cash_flow/server/nest-app/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
