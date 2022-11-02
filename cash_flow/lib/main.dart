1d16c83cf240e37d9413fd8502fcc807113be088
alya1007 <alexandra.konjevic@isa.utm.md>
2022-11-02 12:36:24 +0200

    Added two screens

--- cash_flow/lib/main.dart
+++ cash_flow/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:test1/screens/expenses_screen.dart';
 import 'package:test1/screens/main_screen.dart';
 
-
 void main() {
   runApp(Pocketful());
 }
@@ -16,12 +15,83 @@ class Pocketful extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/':(context) => MainScreen(),
-        '/add':(context) => ExpensesScreen(),
+        '/': (context) => MainScreen(),
+        '/add': (context) => ExpensesScreen(),
       },
       initialRoute: '/',
-      theme: ThemeData(appBarTheme: 
-      AppBarTheme(color: Color.fromARGB(255, 59, 125, 62))),
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(color: Color.fromARGB(255, 59, 125, 62))),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:test1/screens/slash_screen.dart';
+// import 'package:test1/screens/login.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: SlashScr(),
+//       // Login()
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
---
+++ cash_flow/lib/screens/login.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  double screenWidth = WidgetsBinding.instance.window.physicalSize.width;
+  double screenHeight = WidgetsBinding.instance.window.physicalSize.height;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+          Container(
+            width: 270,
+            height: 63,
+            margin: const EdgeInsets.only(top: 200),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                      top: 20.0, right: 40.0, bottom: 20.0, left: 18.0),
+                  backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                  shadowColor: const Color.fromARGB(255, 255, 255, 255),
+                  elevation: 6),
+              icon: Image.asset('assets/images/google_icon.png'),
+              label: const Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 33, 149, 243),
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 52),
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('or another email',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 33, 149, 243))),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0x00ffffff),
+                    shadowColor: Color(0x00ffffff),
+                  )))
+        ])));
+  }
+}
