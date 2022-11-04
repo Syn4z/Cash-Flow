f81018b12fb6a5da968de1967ada812220e8c069
sorin <iatcosorin797@gmail.com>
2022-11-04 14:22:24 +0200

    Change server directory and update .gitignore

--- nest_server/nest-app/src/app.module.ts
+++ nest_server/nest-app/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
 })
 export class AppModule {}
+
