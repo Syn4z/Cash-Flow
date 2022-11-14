9bf6cb5ccb973f975af761fa558b0fd9d47db008
alya1007 <alexandra.konjevic@isa.utm.md>
2022-11-14 14:58:28 +0200

    Message when log out

--- cash_flow/lib/widgets/nextButton.dart
+++ cash_flow/lib/widgets/nextButton.dart
@@ -17,10 +17,10 @@ class nextBtn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Container(
       width: screenWidth * 0.7,
-      height: screenHeight * 0.16,
+      height: screenHeight * 0.08,
       margin: EdgeInsets.only(top: upperMargin),
       child: ElevatedButton(
         onPressed: () {
@@ -41,12 +41,12 @@ class nextBtn extends StatelessWidget {
           }
         },
         style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            // padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
             backgroundColor: const Color.fromARGB(255, 82, 183, 136)),
         child: Text(
           text,
           style: TextStyle(
-            fontSize: screenHeight * 0.065,
+            fontSize: screenHeight * 0.037,
             color: const Color.fromARGB(255, 255, 255, 255),
             fontFamily: 'Inter',
             fontWeight: FontWeight.w700,
