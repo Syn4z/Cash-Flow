585e961b12904738136ad6a5b6f592f95770ac3c
sorin <iatcosorin797@gmail.com>
2022-12-20 16:16:25 +0200

    Add some graphql stuff

--- cash_flow/lib/main.dart
+++ cash_flow/lib/main.dart
@@ -1,44 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:test1/screens/GraphQLConfig.dart';
 import 'package:test1/screens/addExpense.dart';
 import 'package:test1/screens/main.dart';
 import 'package:test1/screens/slash.dart';
 import 'package:test1/screens/login.dart';
 import "package:graphql_flutter/graphql_flutter.dart";
+import 'package:http/http.dart' as http;
 
-void main() {
+void main() async {
+  await initHiveForFlutter();
   runApp(Pocketful());
 }
 
 class Pocketful extends StatelessWidget {
-  const Pocketful({super.key});
+  Pocketful();
 
   @override
   Widget build(BuildContext context) {
-    final HttpLink link = HttpLink("localhost:3000/graphql");
-
-    ValueNotifier<GraphQLClient> client =
-        ValueNotifier(GraphQLClient(cache: InMemoryCache(), link: link));
-
+    ValueNotifier<GraphQLClient> client = GraphQLConfig.graphInit();
     return GraphQLProvider(
-        client: client,
-        child: MaterialApp(
-          home: MainScreen(),
-        ));
-  }
-  /*
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => Slash(),
-        '/add': (context) => AddExpenseScreen(),
-      },
-      initialRoute: '/',
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(color: Color.fromARGB(255, 64, 145, 108)),
-          fontFamily: 'Montserrat'),
+      client: client,
+      child: MaterialApp(
+        title: 'Pocketful WORKING!',
+        home: MainScreen(),
+      ),
     );
   }
-  */
 }
--- cash_flow/lib/screens/login.dart
+++ cash_flow/lib/screens/login.dart
@@ -30,7 +30,7 @@ class _LoginState extends State<Login> {
             child: Image.asset('assets/images/logo.png'),
           ),
           InputBar(
-            text: 'Your email',
+            text: 'Your email', //TO DO need to implement my graphql here
             upperMargin: screenHeight * 0.02,
           ),
           InputBar(
@@ -44,7 +44,7 @@ class _LoginState extends State<Login> {
         ])));
   }
 }
-
+/*
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -53,82 +53,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<dynamic> characters = [];
-  bool _loading = false;
+  @override
+  Widget build(BuildContext context) {
+    final HttpLink httpLink = 
+      HttpLink("http://localhost:3000/graphql");
+  
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient> {
+      GraphQLClient (
+        link: httpLink,
+        cache: OptimisticCache (
+          data
+        )
+      )
+    }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _loading
-          ? const CircularProgressIndicator()
-          : characters.isEmpty
-              ? Center(
-                  child: ElevatedButton(
-                    child: const Text("Fetch Data"),
-                    onPressed: () {
-                      fetchData();
-                    },
-                  ),
-                )
-              : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
-                      itemCount: characters.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: ListTile(
-                            leading: Image(
-                              image: NetworkImage(
-                                characters[index]['image'],
-                              ),
-                            ),
-                            title: Text(
-                              characters[index]['name'],
-                            ),
-                          ),
-                        );
-                      }),
-                ),
-    );
-  }
-
-  void fetchData() async {
-    setState(() {
-      _loading = true;
-    });
-    HttpLink link = HttpLink("http://localhost:3000/graphql");
-    GraphQLClient qlClient = GraphQLClient(
-      link: link,
-      cache: GraphQLCache(
-        store: HiveStore(),
-      ),
-    );
-    QueryResult queryResult = await qlClient.query(
-      QueryOptions(
-        document: gql(
-          """mutation SignUp(\$input: SignUpInput!) {
-  signup(SignUpInput: \$input){
-    accesToken
-    refreshToken
-    user {email username}
-  }
-}""",
-        ),
-      ),
-    );
-
-// queryResult.data  // contains data
-// queryResult.exception // will give what exception you got /errors
-// queryResult.hasException // you can check if you have any exception
-
-// queryResult.context.entry<HttpLinkResponseContext>()?.statusCode  // to get status code of response
-
-    setState(() {
-      characters = queryResult.data!['characters']['results'];
-      _loading = false;
-    });
-  }
+    return Scaffold();
 }
+*/
